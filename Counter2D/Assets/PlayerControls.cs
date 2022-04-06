2387d7c9b7c3469f51a20aad3ff32368af5a1af3
Hestrateja <alvarotakano@gmail.com>
2022-04-06 18:32:21 +0200

    Botones guasones

--- Counter2D/Assets/PlayerControls.cs
+++ Counter2D/Assets/PlayerControls.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] Text ActionText;
+    [SerializeField] Text ButtonText;
     [SerializeField] Text Score;
     [SerializeField] string scoreText;
+    [SerializeField] InteractiveElement up, down, left, right;
     private float scoreNumber;
     private void Start()
     {
@@ -16,7 +18,23 @@ public class PlayerControls : MonoBehaviour
     }
     void Update()
     {
-        
+        ButtonText.text = "IDLE";
+        if (up.isPressed)
+        {
+            ButtonText.text = "UP";
+        }
+        if (down.isPressed)
+        {
+            ButtonText.text = "DOWN";
+        }
+        if (left.isPressed)
+        {
+            ButtonText.text = "LEFT";
+        }
+        if (right.isPressed)
+        {
+            ButtonText.text = "RIGHT";
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
